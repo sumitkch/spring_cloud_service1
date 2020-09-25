e5047af49b23290fd3d698e421798f7fce6ba144
sumitkch <sumitcs0041@gmail.com>
2020-09-25 15:21:40 +0530

    first commit

--- src/main/java/com/example/spring/cloud/service1/SpringCloudService1Application.java
+++ src/main/java/com/example/spring/cloud/service1/SpringCloudService1Application.java
@@ -4,7 +4,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,9 +14,8 @@ import org.springframework.web.bind.annotation.RestController;
 import com.example.spring.cloud.service1.fien.Service2;
 
 @SpringBootApplication
-@EnableDiscoveryClient
+@EnableEurekaClient
 @EnableFeignClients
-
 @RestController
 @RequestMapping("/service1")
 public class SpringCloudService1Application {
@@ -26,14 +26,23 @@ public class SpringCloudService1Application {
 	@Value("${server.port}")
 	private String port;
 	
+	
+	@Value("${my.username}")
+	private String userName;
+	
+	
 	public static void main(String[] args) {
 		SpringApplication.run(SpringCloudService1Application.class, args);
 	}
 	
 	@GetMapping("/serviceDetail")
 	public String getFirstService() {
-		return service2.invokeService2()+" from service1 having port : "+port;
+		return service2.invokeService2()+" from service1 having port : "+port+" username :"+getUserName();
 	}
 	
 
+	@RefreshScope
+	public String getUserName() {
+		return userName;
+	}
 }
